8dadfeadf581531f872c0a94aa82dafafefd339e
JB Silva <julianodavi.jb@hotmail.com>
2022-07-11 06:26:07 -0300

    .

--- Apps/todo_list_provider/lib/app/core/database/sqlite_migration_factory.dart
+++ Apps/todo_list_provider/lib/app/core/database/sqlite_migration_factory.dart
@@ -1,6 +1,5 @@
 import 'package:todo_list_provider/app/core/database/migrations/migration.dart';
-import 'package:todo_list_provider/app/core/database/migrations/migration_v1.dart';
-import 'package:todo_list_provider/app/core/database/migrations/migration_v2.dart';
+import 'package:todo_list_provider/app/core/database/migrations/migration_impl.dart';
 
 class SqliteMigrationFactory {
   List<Migration> getCreateMigration() => [
@@ -10,7 +9,7 @@ class SqliteMigrationFactory {
   List<Migration> getUpgradeMigration(int version) {
     var migrations = <Migration>[];
     if(version == 1){
-      migrations.add(MigrationV2());
+      migrations.add(MigrationImpl()); // Aqui seria a nova migration ou seja atualzação do BD
     }
     return migrations;
   }
